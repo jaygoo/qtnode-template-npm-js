c8de72aea23c00aae822899d71fffc828a998a85
guoyujie <296963166@qq.com>
2019-12-12 21:29:52 +0800

    a

--- entry/index.js
+++ entry/index.js
@@ -5,3 +5,12 @@ export  default () =>{
         c: 3,
     }
 }
+export const a="aaa";
+export const b="bbb";
+export  let  fun =  () =>{
+    return {
+        a: 1,
+        b: 2,
+        c: 3,
+    }
+}
